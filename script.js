0d0e23588e505528e637aa89446fd1f8e95bdb9d
KamilGimpZAC <75314301+KamilGimpZAC@users.noreply.github.com>
2021-06-21 14:34:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://ancient-cliffs-47536.herokuapp.com/v1';
-   const trelloApiRoot = 'https://ancient-cliffs-47536.herokuapp.com/v1';
+   const apiRoot = 'https://ancient-cliffs-47536.herokuapp.com/v1/';
+   const trelloApiRoot = 'https://ancient-cliffs-47536.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
